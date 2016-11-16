11941697e3f0550bb17708d45d0d67ddd137e585
1613010 <1613010@034lab04038.up.edu>
2016-11-16 21:09:24 -0200

    Apagando comentarios Testando comando IF

---
+++ VotacaoPizza/src/Pizza.java
@@ -0,0 +1,16 @@
+
+public class Pizza {
+    private String sabor;
+
+    public String getSabor() {
+        return sabor;
+    }
+
+    public void setSabor(String sabor) {
+        this.sabor = sabor;
+    }
+    
+    
+    
+    
+}
