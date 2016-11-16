d75f6655642321e6dd9981ff19ff56d0e73f723f
Administrador <Administrador@034lab04044.up.edu>
2016-11-16 19:56:15 -0200

    Apagando comentarios desnecessários

--- VotacaoPizza/src/votacaopizza/VotacaoPizza.java
+++ VotacaoPizza/src/votacaopizza/VotacaoPizza.java
@@ -1,15 +1,9 @@
 package votacaopizza;
 
-/**
- *
- * @author Administrador
- */
 
 public class VotacaoPizza {
 
-    /**
-     * @param args the command line arguments
-     */
+
     public static void main(String[] args) {
         // TODO code application logic here
     }
